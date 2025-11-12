046551b72e5939dc2218bfe31c103a6dc037f670
raymondfanuel <raymondfanuel1@gmail.com>
2025-11-12 22:26:14 +0300

    navbar

--- src/pages/Michezo/Michezo.jsx
+++ src/pages/Michezo/Michezo.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+import DevFooter from '../../components/DevFooter/DevFooter'
 
 const Michezo = () => {
   return (
     <div>
+      <Navbar />
       Michezo page
+      <Footer />
+      <DevFooter />
     </div>
   )
 }
