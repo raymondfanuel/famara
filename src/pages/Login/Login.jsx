f76072c14af5e7fb6bcd548d55cec8ebd94d324a
Decool-web <fitnessdecool@gmail.com>
2025-11-13 10:48:50 +0300

    modified Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import  styles from "./Login.module.css";
+import { assets } from '../../assets/assets'
 
 const Login = () => {
-  return (
-    <div>
-      Login Page
+return(
+    <div className={styles.body}>
+    <div className={styles.container}>
+      <div className={styles.login_box}>
+        <h2><img src={assets.famara_pic} alt="" />Login</h2>
+        <form action="#">
+          <div className={styles.input_box}>
+            <input type="email" required />
+            <label>Email</label>
+          </div>
+          <div className={styles.input_box}>
+            <input type="password" required />
+            <label>Password</label>
+          </div>
+          <div className={styles.forgot_pass}>
+            <a href="#">Forgot your password?</a>
+          </div>
+          <button type="submit" className={styles.btn}>Login</button>
+          <div className={styles.signup_link}>
+            <a href="#">Signup</a>
+          </div>
+        </form>
+      </div>
+      {Array.from({length: 50}, (_, i)=>(
+        <span key={i} style={{"--i": i}}></span>
+      ))}
     </div>
-  )
+    </div>
+)
 }
 
 export default Login
